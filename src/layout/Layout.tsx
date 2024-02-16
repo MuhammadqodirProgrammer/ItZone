5ad25d4390b463c4035148c5c05b53b60e222177
MuhammadqodirProgrammer <115607323+MuhammadqodirProgrammer@users.noreply.github.com>
2024-02-16 20:24:18 +0500

    bag fixed

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -5,7 +5,7 @@ import Navbar from './Navbar/Navbar';
 
 export default function Layout({ children }: any) {
 	return (
-		<div>
+		<div className="main_box">
 			<Header />
 			<div className='inner_container mt-[12vh] min-h-[78vh] '>{children}</div>
 			<Footer />
